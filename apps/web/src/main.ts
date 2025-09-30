454b954a58ae7cbf1c52a1914d90545e8a8ef783
FireOnIcecube <fireonicecube@gmail.com>
2025-09-30 17:56:28 +0800

    feat: 添加对 Markdown 文件的支持并更新依赖项
    - 在 env.d.ts 中声明对 .vue 和 .md 文件的模块支持
    - 在 main.ts 中引入并使用 @unhead/vue 进行头部管理
    - 更新 package.json 以包含 @pkg/content 依赖
    - 新增 Test.md 文件作为示例文章
    - 移除不再使用的 @vueuse/head 依赖

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -3,12 +3,17 @@ import './assets/main.css'
 import App from './App.vue'
 import { ViteSSG } from 'vite-ssg'
 import { createPinia } from 'pinia'
-import { routes } from 'vue-router/auto-routes' // 改成你拆分出的 routes 檔案
+import { routes } from 'vue-router/auto-routes'
+import { createHead } from '@unhead/vue/client'
 
 export const createApp = ViteSSG(App, { routes }, ({ app, router, initialState }) => {
-  // 這裡可以註冊 Pinia
+  // Pinia
   const pinia = createPinia()
   app.use(pinia)
+
+  // unhead
+  const head = createHead()
+  app.use(head)
 
   // 如果有其他 plugin 也可以在這裡用
   // app.use(SomeOtherPlugin)
---
+++ packages/content/articles/Test.md
@@ -0,0 +1,10 @@
+---
+title: My Cool App
+meta:
+  - name: description
+    content: Hello World
+---
+
+# 這是第一篇文章
+
+第一篇內文
