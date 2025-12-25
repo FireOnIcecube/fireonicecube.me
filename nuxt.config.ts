453daa6945e7cdfb7576f46b3ad34e3ec057c8a8
FireOnIcecube <fireonicecube@gmail.com>
2025-12-26 07:03:39 +0800

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/hints',
+    '@nuxt/icon',
+    '@nuxt/image',
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-04-03',
+})
