abe11302a62abc8ba66f9ffd302969b87b697508
FireOnIcecube <fireonicecube@gmail.com>
2025-12-28 21:17:08 +0800

    chore: add @iconify/vue dependency

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,18 @@
+<script lang="ts" setup>
+import { Icon } from "@iconify/vue";
+</script>
+
 <template>
-  <div class="flex flex-col">
-    <div>Header</div>
-    <slot />
-    <div>Footer</div>
+  <div class="bg-surface flex min-h-dvh flex-col">
+    <header class="bg-component flex items-center justify-between px-2 py-4">
+      <div>LOGO</div>
+      <Icon icon="ci:hamburger-lg" class="text-primary size-8" />
+    </header>
+    <main class="flex-1">
+      <slot />
+    </main>
+    <footer class="bg-primary text-center">
+      <p class="text-ink-alt py-2 text-sm">©2025 FireonIcecube</p>
+    </footer>
   </div>
 </template>
