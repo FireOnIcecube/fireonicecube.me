7c5fac180c42977fd70d0172d3da6475d8f7f9e8
FireOnIcecube <fireonicecube@gmail.com>
2026-01-02 20:37:16 +0800

    style: change mobile header & footer padding

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,7 +5,7 @@ import { Icon } from "@iconify/vue";
 <template>
   <div class="bg-surface flex flex-col min-h-dvh">
     <header
-      class="bg-component px-4 py-3 flex gap-x-4 max-h-[80px] items-center justify-between"
+      class="bg-component px-6 py-3 flex gap-x-4 max-h-[80px] items-center justify-between"
     >
       <div class="flex gap-x-0.5 items-center">
         <div class="shrink-0 size-10">
@@ -25,13 +25,13 @@ import { Icon } from "@iconify/vue";
         </div>
       </div>
 
-      <Icon icon="ci:hamburger-lg" class="text-primary mt-2 shrink-0 size-10" />
+      <Icon icon="ci:hamburger-lg" class="text-primary mt-2 shrink-0 size-9" />
     </header>
     <main class="flex-1">
       <slot />
     </main>
-    <footer class="mt-12 text-center bg-primary">
-      <p class="text-ink-alt text-sm py-2">©2026 FireonIcecube</p>
+    <footer class="mt-12 py-2 text-center bg-primary">
+      <p class="text-ink-alt text-sm">©2026 FireonIcecube</p>
     </footer>
   </div>
 </template>
