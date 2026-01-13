157b3ee7e5bf72af079a671345c2cbb2c4b4ae6e
FireOnIcecube <fireonicecube@gmail.com>
2026-01-13 21:02:35 +0800

    feat(blog): add BlogCard component for displaying blog posts

---
+++ app/components/BlogCard.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+interface Props {
+  title: string;
+  dateTime: Date;
+  tags: string[];
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <section class="text-ink-muted p-4 cursor-pointer border">
+    <h1 class="text-justify">My First Post</h1>
+    <div class="mt-4 flex gap-x-4 items-end justify-between">
+      <div class="flex gap-x-2 overflow-hidden">
+        <div class="px-2 py-1 bg-red-200">123</div>
+        <div class="px-2 py-1 bg-red-200">123</div>
+        <div class="px-2 py-1 bg-red-200">123</div>
+        <div class="px-2 py-1 bg-red-200">123</div>
+        <div class="px-2 py-1 bg-red-200">123</div>
+      </div>
+      <time class="shrink-0">2026/01/12</time>
+    </div>
+  </section>
+</template>
