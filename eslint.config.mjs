34f57fa527053d67420512b178159790d6f4f65f
FireOnIcecube <fireonicecube@gmail.com>
2026-01-15 05:00:59 +0800

    chore(eslint): update ESLint configuration and add necessary parsers
    - Added vue-eslint-parser and @typescript-eslint/parser to ESLint config.
    - Updated ESLint rules for Vue files to enhance linting capabilities.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,26 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 import unocss from "@unocss/eslint-config/flat";
+import vueParser from "vue-eslint-parser";
+import tsParser from "@typescript-eslint/parser";
 import tseslint from "typescript-eslint";
 
 export default withNuxt([
   unocss,
-  tseslint.configs.recommended,
+  ...tseslint.configs.recommended,
 
   {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: [".vue"],
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
     rules: {
-      // allow element self-closing
       "vue/html-self-closing": "off",
     },
   },
